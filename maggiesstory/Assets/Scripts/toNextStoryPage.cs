3aa9190687e88ed5688c79174a10e51637ae5aa2
mdgrey <madelinedg@gmail.com>
2017-03-28 11:32:22 -0400

    putting all our files up!

---
+++ maggiesstory/Assets/Scripts/toNextStoryPage.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class toNextStoryPage: MonoBehaviour { 
+
+	// Use this for initialization
+	public void nextScene (int sceneNum) {
+		//yield return new WaitForSeconds (1.0f); 
+
+		Application.LoadLevel (sceneNum); 	
+		Debug.Log ("page1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
