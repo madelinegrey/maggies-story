ca81f96db585418e78ba7cc126f28aa312ec4365
mdgrey <madelinedg@gmail.com>
2017-04-02 22:35:26 -0400

    added auto typing phase 1

---
+++ maggiesstory/Assets/AutoType.cs
@@ -0,0 +1,34 @@
+﻿// base script receieved from http://wiki.unity3d.com/index.php?title=AutoType
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AutoType : MonoBehaviour {
+
+	public float letterPause = 0.2f;
+	public AudioClip sound;
+	private Text txt {
+		get {
+			return GetComponent<Text> ();
+		}
+	}
+
+
+	string message;
+
+	// Use this for initialization
+	void Start () {
+		message = txt.text;
+		txt.text = "";
+		StartCoroutine(TypeText ());
+	}
+
+	IEnumerator TypeText () {
+		foreach (char letter in message.ToCharArray()) {
+			txt.text += letter;
+			yield return 0;
+			yield return new WaitForSeconds (letterPause);
+		}      
+	}
+}
