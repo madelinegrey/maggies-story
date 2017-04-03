8382a677dccc0650dc275e6fe120c94c67ac8ddc
Roxanne <plow@conncoll.edu>
2017-04-02 22:29:50 -0400

    Puzzle 2
    collecting falling objects

---
+++ maggiesstory/Assets/Scripts/Bucket.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bucket : MonoBehaviour {
+	public Gamer _gamer; 
+	//public AudioClip _audio;
+	// Use this for initialization
+
+
+	public string leftKey = "left";
+	public string rightKey = "right";
+	public string upKey = "up";
+	public string downKey = "down";
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	/*
+	void OnCollisionEnter2D(Collision2D collision){
+		
+		if (collision.gameObject.tag == "Block") {
+			//_gamer.Addbucket ();
+			//AudioSource.PlayClipAtPoint (_audio, transform.position);
+			Debug.Log ("hit block");
+		} else {
+			Debug.Log ("wrong");
+		}*/
+		
+
+
+}
+
